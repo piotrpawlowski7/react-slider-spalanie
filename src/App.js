bb35054bde037d58549f6eb230ca1f9dec6c6ce1
piotrpawlowski7 <ppawlowski7@gmail.com>
2022-08-03 21:54:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactSlider from 'react-slider'
+
+
+
+function App() {
+console.log()
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+        <ReactSlider 
+    className="horizontal-slider"
+    thumbClassName="example-thumb"
+    trackClassName="example-track"
+    renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+/>
+       
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
